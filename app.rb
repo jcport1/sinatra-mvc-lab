e0cc00e2029058b650d5881aed4bd95e830c2ee8
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-03 15:06:08 -0500

    commit tree d8b549cdc73ffb73b649f985d15828ed31271382

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+
+    get "/" do
+
+        erb :user_input 
+
+    end
+
+    post "/piglatinize" do
+
+        @text = params[:words]
+        @pig_latin = PigLatinizer.new(text_from_user)
+
+        erb :piglatinize
+
+    end 
+
 end
